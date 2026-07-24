14238c0e3ada6b2b2be0dc0a9d290aea5cf14e2f
TOPyingsi <60088087+TOPyingsi@users.noreply.github.com>
2026-07-24 11:41:18 +0800

    s

--- assets/Game_Bundles/73_ZRSJZ_DLC_BNS/Script/Panel/ZRSJZ_BNS_UpLevelPanel.ts
+++ assets/Game_Bundles/73_ZRSJZ_DLC_BNS/Script/Panel/ZRSJZ_BNS_UpLevelPanel.ts
@@ -1,11 +1,28 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, EventTouch, Node } from 'cc';
+import { ZRSJZ_Panel } from '../../../73_ZRSJZ/Scripts/Panel/ZRSJZ_Panel';
+import { ZRSJZ_UIManager } from '../../../73_ZRSJZ/Scripts/Manager/ZRSJZ_UIManager';
+import { ZRSJZ_PANEL } from '../../../73_ZRSJZ/Scripts/ZRSJZ_Constant';
 const { ccclass, property } = _decorator;
 
 @ccclass('ZRSJZ_BNS_UpLevelPanel')
-export class ZRSJZ_BNS_UpLevelPanel extends Component {
+export class ZRSJZ_BNS_UpLevelPanel extends ZRSJZ_Panel {
+
+    OnButtonClick(event: EventTouch) {
+        switch (event.getCurrentTarget().name) {
+            case "关闭":
+                ZRSJZ_UIManager.Instance.HidePanel(ZRSJZ_PANEL.避难所_升级界面);
+                break;
+            case "升级":
+                this.UpLevelClick();
+                break;
+        }
+
+    }
+
+    UpLevelClick() {
 
 
-
+    }
 }
 
 
---
+++ assets/Game_Bundles/73_ZRSJZ_DLC_BNS/Script/ZRSJZ_BNS_Arrows.ts
@@ -0,0 +1,12 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('ZRSJZ_BNS_Arrows')
+export class ZRSJZ_BNS_Arrows extends Component {
+    start() {
+
+    }
+
+}
+
+
