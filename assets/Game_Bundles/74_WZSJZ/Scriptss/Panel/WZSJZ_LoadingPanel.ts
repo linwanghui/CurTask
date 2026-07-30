0f66e873d8fa638a5a0051d76bf82a94fdd76e91
TOPyingsi <60088087+TOPyingsi@users.noreply.github.com>
2026-07-30 20:00:40 +0800

    s

---
+++ assets/Game_Bundles/74_WZSJZ/Scriptss/Panel/WZSJZ_LoadingPanel.ts
@@ -0,0 +1,47 @@
+import { _decorator, Component, director, Label, Node, Sprite } from 'cc';
+import { PanelBase } from '../../../../Scripts/Framework/UI/PanelBase';
+import { WZSJZ_UIManager } from '../WZSJZ_UIManager';
+import { WZSJZ_Constant } from '../WZSJZ_Constant';
+
+const { ccclass, property } = _decorator;
+
+@ccclass('WZSJZ_LoadingPanel')
+export class WZSJZ_LoadingPanel extends PanelBase {
+    @property(Sprite)
+    LoadingFG: Sprite = null;
+    @property(Label)
+    LoadingLabel: Label = null;
+    //第一个参数为要转跳的场景
+    Show(...args: any[]): void {
+        WZSJZ_UIManager.Instance.HideAllPanel();
+        this.node.active = true;
+        // this.LoadingLabel.string = `正在加载：${0}%`;
+
+        const loadScene = (senceName, bundleName = null) => {
+            director.loadScene(senceName, () => {
+                this.scheduleOnce(() => {//延迟0.5关闭界面
+                    WZSJZ_UIManager.Instance.HidePanel(WZSJZ_Constant.Panel.LoadingPanel);
+                }, 1);
+            });
+            director.preloadScene(senceName, (completedCount: number, totalCount: number, item: any) => {
+                if (this.LoadingFG) {
+                    this.LoadingFG.fillRange = this.LoadingFG.fillRange > completedCount / totalCount ? this.LoadingFG.fillRange : completedCount / totalCount;
+                }
+                if (this.LoadingLabel) {
+                    this.LoadingLabel.string = `正在加载：${Math.ceil(completedCount / totalCount * 100)}%`;
+                }
+            }, () => {
+            });
+        }
+        if (args[0]) {
+            loadScene(args[0]);
+        }
+    }
+
+    Hide(endCb: Function = null): void {
+        this.node.active = false;
+        endCb && endCb();
+    }
+}
+
+
---
+++ assets/Game_Bundles/74_WZSJZ/Scriptss/WZSJZ_Constant.ts
@@ -0,0 +1,13 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('WZSJZ_Constant')
+export class WZSJZ_Constant {
+    public static Panel = {
+        LoadingPanel: "Panel/LoadingPanel",//加载界面
+
+    }
+
+}
+
+
